f79611f0a74189b99e2185eaf1dcbe2586a1ecd3
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2023-12-03 03:00:27 +0700

    add new use case

---
+++ Restaurant/domain/use_case/UserUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Restaurant.repository;
+
+namespace Restaurant.domain.use_case;
+
+public class UserUseCase
+{
+    private readonly UserRepository _userRepository;
+
+    public UserUseCase(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public void Execute(User executor, User newUser)
+    {
+        // Проверка роли текущего пользователя
+        if (executor.UserRole.UserRoleName != "ADMIN")
+        {
+            throw new InvalidOperationException("Only administrators can add new users.");
+        }
+
+        // Проверка роли нового пользователя
+        if (newUser.UserRole.UserRoleName == "ADMIN" && executor.UserRole.UserRoleName != "ADMIN")
+        {
+            throw new InvalidOperationException("Only administrators can add new administrators.");
+        }
+
+        // Добавление пользователя
+        _userRepository.AddUser(newUser);
+    }
+}
